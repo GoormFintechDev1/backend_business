5326c8112942bcf0c4011be32d0075a447c48f6d
AN TAEO <hantive98@gmail.com>
2024-12-09 10:31:42 +0900

    Create data.sql

---
+++ src/main/resources/data.sql
@@ -0,0 +1,10 @@
+INSERT INTO business_registration (
+    business_registration_id, member_id, business_type, business_item, br_num,
+    address, business_start_date, representative_name, company_name, pos_id, account_id
+) VALUES
+      (1, NULL, '음식점업', '커피 전문점', '3815500350',
+       '서울특별시 서대문구 연희동 134-28 인준빌딩 1층', '2019-12-01', '김민재', '카페연희', NULL, NULL),
+      (2, NULL, '음식점업', '커피 전문점', '2068692418',
+       '서울특별시 서대문구 연희동 96-9', '2020-03-15', '박수진', '프로토콜 커피', NULL, NULL),
+      (3, NULL, '음식점업', '커피 전문점', '2768956781',
+       '서울특별시 서대문구 연희동 132-24', '2021-06-20', '이정훈', '유니크 커피로스터스', NULL, NULL);
